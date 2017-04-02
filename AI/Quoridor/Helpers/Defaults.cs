7b1e5bd87685a2573caea609a59eb15d6cd906b2
Raiondesu <a.ishakov@innopolis.ru>
2017-04-02 17:16:26 +0300

    Added default size

--- AI/Quoridor/Helpers/Defaults.cs
+++ AI/Quoridor/Helpers/Defaults.cs
@@ -3,6 +3,7 @@ namespace Quoridor
 	public static class Defaults
 	{
 		public const int WallsAmount = 10;
+		public const int BoardSize = 9;
 		public static readonly System.Type Algorithm = typeof(Algorithms.MinMax);
 	}
 }
